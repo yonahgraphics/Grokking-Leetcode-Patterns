7897e2fcb176ccf25fce7a996f0aa339d54fe81b
Yonah Byarugaba <40076669+yonahgraphics@users.noreply.github.com>
2022-03-09 00:26:45 +0200

    Added the leetcode link

--- Binary Trees/invertBST.py
+++ Binary Trees/invertBST.py
@@ -4,6 +4,8 @@ Easy
 Given the root of a binary tree, invert the tree, and return its root.
 Input: root = [4,2,7,1,3,6,9]
 Output: [4,7,2,9,6,3,1]
+
+Leetcode Link: https://leetcode.com/problems/invert-binary-tree/
 '''
 # Definition for a binary tree node.
 # class TreeNode:
@@ -15,7 +17,7 @@ class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         if not root:
             return None
-            
+
         self.invertTree(root.left)
         self.invertTree(root.right)
         root.left, root.right = root.right, root.left
