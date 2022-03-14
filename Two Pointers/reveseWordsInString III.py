fa6a78fac5cd437dd0e1c6767436b49d75e3e710
Yonah Byarugaba <40076669+yonahgraphics@users.noreply.github.com>
2022-03-14 21:19:14 +0200

    Added description

--- Two Pointers/reveseWordsInString III.py
+++ Two Pointers/reveseWordsInString III.py
@@ -1,4 +1,14 @@
+'''
+Leetcode Link: https://leetcode.com/problems/reverse-words-in-a-string-iii/
 
+557. Reverse Words in a String III
+Given a string s, reverse the order of characters in each 
+word within a sentence while still preserving whitespace and initial word order.
+
+Example 1:
+Input: s = "Let's take LeetCode contest"
+Output: "s'teL ekat edoCteeL tsetnoc"
+'''
 def reverseWords(s):
     result = ""
     
