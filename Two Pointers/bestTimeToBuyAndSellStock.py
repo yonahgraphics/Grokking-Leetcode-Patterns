1cf7da6d12b61e485b84776bc4233e5c69e354b6
Yonah Byarugaba <40076669+yonahgraphics@users.noreply.github.com>
2022-03-17 13:03:11 +0200

    Added best time to buy and sell

---
+++ Two Pointers/bestTimeToBuyAndSellStock.py
@@ -0,0 +1,31 @@
+'''
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+You want to maximize your profit by choosing a single day to buy one stock and choosing a 
+different day in the future to sell that stock.
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any 
+profit, return 0.
+'''
+# Tme Complexity: O(n) b'se we scan thru the array once albeit with two pointers.
+# Space Complxity: O(1), no auxilliary space used.
+def maxProfit(prices):
+    """
+    :type prices: List[int]
+    :rtype: int
+    """
+    i = 0 # buy
+    j = 1 # sell
+    
+    max_profit = 0
+    for j in range(len(prices)):
+        curr_profit = prices[j] - prices[i]
+        if curr_profit <= 0: # not making profits
+            i = j # buy another day
+        max_profit = max(max_profit, curr_profit) # set max profit
+    return max_profit
+
+if __name__ == "__main__":
+    print(maxProfit([1,2,7,9,4,3,3]))
+        
+    
+    
+    
