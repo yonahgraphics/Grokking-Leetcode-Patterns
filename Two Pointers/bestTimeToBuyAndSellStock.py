f21e6f853616264e2faa34d8dfc475a121de5925
Yonah Byarugaba <40076669+yonahgraphics@users.noreply.github.com>
2022-03-17 13:43:25 +0200

    Added the naive approach

--- Two Pointers/bestTimeToBuyAndSellStock.py
+++ Two Pointers/bestTimeToBuyAndSellStock.py
@@ -5,8 +5,22 @@ different day in the future to sell that stock.
 Return the maximum profit you can achieve from this transaction. If you cannot achieve any 
 profit, return 0.
 '''
+
+
+# Tme Complexity: O(n*2) b'se we scan thru the array once albeit with two pointers.
+# Space Complxity: O(1), no auxilliary space used.
+
+def maxProfit1(prices):
+    max_profit = 0
+    for i in range(len(prices)):
+        for j in range(i+1, len(prices)):
+            curr_profit = prices[j] - prices[i]
+            max_profit = max(max_profit, curr_profit)
+    return max_profit
+                            
 # Tme Complexity: O(n) b'se we scan thru the array once albeit with two pointers.
 # Space Complxity: O(1), no auxilliary space used.
+
 def maxProfit(prices):
     """
     :type prices: List[int]
@@ -25,6 +39,7 @@ def maxProfit(prices):
 
 if __name__ == "__main__":
     print(maxProfit([1,2,7,9,4,3,3]))
+    print(maxProfit1([1,2,7,9,4,3,3]))
         
     
     
