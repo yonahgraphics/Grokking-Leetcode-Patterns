80e5136de2d6f432066774ddf0f5e5d9ac4d5c55
Yonah Byarugaba <40076669+yonahgraphics@users.noreply.github.com>
2022-03-19 09:26:45 +0200

    Added alternative solutions for ceiling

--- Binary Search/ceilingOfNumber.py
+++ Binary Search/ceilingOfNumber.py
@@ -12,8 +12,37 @@ Example 2:
 
 Input: nums = [1,3,5,6], target = 2
 Output: 1
+
+***PRO TIP***
+
+If you use a recursive approach, then at each stage, you have to make a recursive call.
+That means leaving the current invocation on the stack, and calling a new one. 
+When you're k levels deep, you've got k lots of stack frame, so the space complexity ends 
+up being proportional to the depth you have to search.
+With your iterative code, you're allocating one variable (O(1) space) plus a single stack 
+frame for the call (O(1) space). Your while loop doesn't ever allocate anything extra, either 
+by creating new variables or object instances, or by making more recursive calls. So the only 
+space you need, for your whole call, is the O(1) space taken up by the variable you create and the rest of the stack frame.
+Whenever you can rewrite a recursive algorithm as a simple iteration, it's worth doing precisely 
+because of this reduced space requirement.
 '''
-def searchInsert(nums, target):
+
+
+# Time Compleexity: O(n)
+# Space Complexity: O(1)
+def searchInsert1(nums, key):
+    for i in range(len(nums)):
+        if key == nums[i]:
+            return i
+        elif key > nums[i]:
+            continue
+        else:
+            return i
+
+
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+def searchInsert2(nums, target):
     """
     :type nums: List[int]
     :type target: int
@@ -21,7 +50,6 @@ def searchInsert(nums, target):
     """
     low = 0
     high = len(nums)-1
-    found = False
     
     while low <= high:
         mid = low  + (high - low)//2
@@ -34,7 +62,23 @@ def searchInsert(nums, target):
             return mid
     return low
 
+# Time Complexity: O(logn)
+# Space Complexity: O()
+def getCeilingOptimum3(array, key, low, high):
+    if low > high:
+        return low
+    mid = low + (high - low)//2
+    if key == array[mid]:
+        return mid
+    if key < array[mid]:
+       high = mid-1
+       return getCeilingOptimum(array, key, low, high)
+    else:
+       low = mid+1
+       return getCeilingOptimum(array, key, low, high)
+    
+
 if __name__ == "__main__":
-    print(searchInsert([1,2,3,4,7,8], 9))
+    print(searchInsert1([1,2,3,4,7,8], 7))
             
         
