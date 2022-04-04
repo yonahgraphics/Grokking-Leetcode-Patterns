15080865589bf7f9a415ecd82dae74af988a878b
Yonah Byarugaba <40076669+yonahgraphics@users.noreply.github.com>
2022-04-05 01:19:56 +0200

    printLinkedlist

---
+++ LinkedLists/linkedList.py
@@ -0,0 +1,40 @@
+class Node:
+    def __init__(self, val = None):
+        self.val = val
+        self.next = None
+
+
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+def printListIterative(head):
+    curr = head
+    while curr is not None:
+        print(curr.val)
+        curr = curr.next
+    
+
+def printListRecursive(head):
+    if head is None:
+        return
+    print(head.val)
+    printListRecursive(head.next)
+
+
+if __name__ == "__main__":
+    a = Node('A')
+    b = Node('B')
+    c = Node('C')
+    d = Node('D')
+    e = Node('E')
+    f = Node('F')
+
+    a.next = b
+    b.next = c
+    c.next = d
+    d.next = e
+    e.next = f
+
+    # printListIterative(a)
+    printListRecursive(a)
+
+    
