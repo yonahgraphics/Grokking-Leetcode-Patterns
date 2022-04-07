3ec4eb225a8d7298e337c9def81236579c72f5ad
Yonah Byarugaba <40076669+yonahgraphics@users.noreply.github.com>
2022-04-07 12:14:50 +0200

    LinkedList Cycle

---
+++ Fast and Slow pointers/palindromeLinkedList.py
@@ -0,0 +1,26 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+
+#Time complexity O(n)
+# Space complexity: O(n)
+def isPalindrome(head):
+    curr =  head
+    nodes_values = []
+    while curr is not None:
+        nodes_values.append(curr.val)
+        curr = curr.next
+    
+    i, j = 0, len(nodes_values)-1
+    
+    while i < j:
+        if nodes_values[i] != nodes_values[j]:
+            return False
+        i += 1
+        j -= 1
+    return True
+        
+        
