25d0910f1d26540e3a64c9a8230897b8f8753e89
Yonah Byarugaba <40076669+yonahgraphics@users.noreply.github.com>
2022-04-08 14:26:52 +0200

    Added: removeDuplicatesFromSOrtedList

---
+++ Fast and Slow pointers/removeDuplicatesFromSortedList.py
@@ -0,0 +1,6 @@
+"""
+Link: https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+Remove Duplicates from Sorted List
+Given the head of a sorted linked list, delete all duplicates such that 
+each element appears only once. Return the linked list sorted as well.
+"""
