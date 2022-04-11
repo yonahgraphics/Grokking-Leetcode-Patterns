82d618da62645553fb4202b19140cbc0cbae35dc
Yonah Byarugaba <40076669+yonahgraphics@users.noreply.github.com>
2022-04-11 22:36:38 +0200

    Added rotate Array

---
+++ Arrays/rotateArray.py
@@ -0,0 +1,38 @@
+"""
+https://leetcode.com/problems/rotate-array/
+
+189. Rotate Array
+
+Given an array, rotate the array to the right by k steps, where k is non-negative.
+Example 1:
+
+Input: nums = [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+Example 2:
+
+Input: nums = [-1,-100,3,99], k = 2
+Output: [3,99,-1,-100]
+Explanation: 
+rotate 1 steps to the right: [99,-1,-100,3]
+rotate 2 steps to the right: [3,99,-1,-100]
+"""
+
+def rotate(self, nums, k):
+    k = k % len(nums)     #take care of the case where k >= len(nums)  
+    nums[:] = nums[-k:] + nums[:-k] 
+    return nums
+
+
+def rotate(self, nums, k):
+    res = [0]*len(nums)
+    for i in range(len(nums)):
+        shiftIndex = (i + k)%len(nums)
+        # print("shifted: ", shiftIndex, "val", nums[i])
+        res[shiftIndex] = nums[i]
+    nums = res
+    return nums
+
