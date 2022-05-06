1603fc4bd314720e1d525ad6d309eb4ea8cd2c55
Yonah Byarugaba <40076669+yonahgraphics@users.noreply.github.com>
2022-05-06 04:08:32 +0200

    Added: bubble sort

--- Sorting/bubbleSort.py
+++ Sorting/bubbleSort.py
@@ -8,10 +8,16 @@ and swapping them whenever they don't matching the sorting order.
 """
 # Time complexity: O(n^2)
 # Space complexity: O(1)
-def selectionSort(nums):
+def bubbleSort(nums):
     for i in range(len(nums)-1):
+        isSorted = True
         for j in range(i + 1, len(nums)):
             if nums[j] < nums[i]:
                 nums[i], nums[j] = nums[j], nums[i]
+                isSorted = False 
+        if isSorted: break # If we receive a sorted list, we really dnt want to sort it
+        # We can use a flag to detect if it is sort and break out of the loop, reducing the 
+        # time complexity to O(n)
+            
     return nums
 print(selectionSort([4,5,2,3]))
