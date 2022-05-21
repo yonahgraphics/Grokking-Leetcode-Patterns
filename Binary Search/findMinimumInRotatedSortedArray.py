59da95ac32b74e5830e351fc1107c9f933789a55
Yonah Byarugaba <40076669+yonahgraphics@users.noreply.github.com>
2022-05-21 23:25:33 +0200

    Added: Find minimum in rotated sorted array

---
+++ Binary Search/findMinimumInRotatedSortedArray.py
@@ -0,0 +1,45 @@
+"""
+153. Find Minimum in Rotated Sorted Array
+Suppose an array of length n sorted in ascending order is rotated between 1 and n times. 
+For example, the array nums = [0,1,2,4,5,6,7] might become:
+
+[4,5,6,7,0,1,2] if it was rotated 4 times.
+[0,1,2,4,5,6,7] if it was rotated 7 times.
+Notice that rotating an array [a[0], a[1], a[2], ..., a[n-1]] 1 time results in the array 
+[a[n-1], a[0], a[1], a[2], ..., a[n-2]].
+
+Given the sorted rotated array nums of unique elements, return the minimum element of this array.
+
+You must write an algorithm that runs in O(log n) time.
+Example 1:
+
+Input: nums = [3,4,5,1,2]
+Output: 1
+Explanation: The original array was [1,2,3,4,5] rotated 3 times.
+"""
+
+
+# Time complexity: O(logn)
+# Space complexity: O(1)
+def findMin(nums):
+    low = 0
+    high = len(nums) - 1
+    minimum = nums[0]
+    
+    while low <= high:
+        # If the array is sorted well, or rotated n time
+        if nums[high] >= nums[low]:
+            return min(minimum, nums[low])
+        
+        mid = low + (high - low)//2
+        minimum = min(minimum, nums[mid])
+        
+        # In left sorted portion
+        if nums[mid] >= nums[low]:
+            low = mid + 1
+            
+        # In right sorted porting
+        else:
+            high = mid - 1
+    return minimum
+    
