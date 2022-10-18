f56a04ffe0aac36c2d38d9b1cef13725bb43b49b
Yonah Byarugaba <40076669+yonahgraphics@users.noreply.github.com>
2022-10-18 17:36:29 +0200

    corrected the time and space complexity on 'isSameTree'

--- Binary Trees/sameTree.py
+++ Binary Trees/sameTree.py
@@ -6,23 +6,23 @@
 #         self.right = right
 class Solution(object):
 
-#Time complexity : O(N), where N is a number of nodes in the tree, 
+#Time complexity : O(p+q), where p+q is a number of nodes in the two trees,
 # since one visits each node exactly once.
 
-# Space complexity : O(log(N)) in the best case of completely balanced 
-# tree and O(N) in the worst case of completely unbalanced tree, 
+# Space complexity : O(log(p+q)) in the best case of completely balanced
+# tree and O(p+q) in the worst case of completely unbalanced tree,
 # to keep a recursion stack.
 
     def isSameTree(self, p, q):
         if not p and not q:
             return True
-        
+
         if not p and q or not q and p:
             return False
-        
+
         if p.val != q.val:
             return False
-        
+
         isLeftSame = self.isSameTree(p.left, q.left)
         isRightSame = self.isSameTree(p.right, q.right)
         return isLeftSame and isRightSame
